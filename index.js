78ba7b1c8d96f0eb00d01bd8115084c0a8c90ad9
Mihir-bhr-16 <mihirbihari16@gmail.com>
2026-03-09 23:08:58 +0530

    added dotenv dependency

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 3000
+const json = {
+"employee":{"name":"John", "age":30, "city":"New York"}
+}
 app.get('/',(req,res) => {
     res.send('Hello World!')
 })
 app.get('/twitter',(req,res)=>{
     res.send('Hi')
+})
+app.get('/github',(req,res)=>{
+    res.json(json)
 })
 app.get('/login',(req,res)=>{
     res.send('<h1> Enter ypur login details')
